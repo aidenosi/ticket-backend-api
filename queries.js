7ba47f93091370983730844c13d6ecf2be50688f
aidenosi <aidenosi@yahoo.ca>
2019-07-09 16:26:14 -0400

    Changes made to accommodate for editing functionality. Also fixed issue with creating records with null values, now columns that can be null will be set to null if no input is given.

--- queries.js
+++ queries.js
@@ -41,8 +41,8 @@ const createTicket = (request, response) => {
   const {
     name,
     email,
-    phoneNum,
-    ext,
+    phone,
+    extension,
     summary,
     type,
     priority,
@@ -52,12 +52,12 @@ const createTicket = (request, response) => {
   } = request.body;
 
   pool.query(
-    "INSERT INTO tickets (name, email, phoneNum, ext, summary, type, priority, category, subcategory, details) VALUES ($1, $2)",
+    "INSERT INTO tickets (name, email, phone, extension, summary, type, priority, category, subcategory, details) VALUES ($1, $2, NULLIF($3,''), NULLIF($4,'')::smallint, $5, $6, $7, $8, $9, $10)",
     [
       name,
       email,
-      phoneNum,
-      ext,
+      phone,
+      extension,
       summary,
       type,
       priority,
@@ -69,8 +69,7 @@ const createTicket = (request, response) => {
       if (error) {
         throw error;
       }
-      response.status(201).send(`User added with ID: ${results.insertId}`);
-      console.log(results.insertId);
+      response.status(201).send(`Ticket created with ID: ${results.insertId}`);
     }
   );
 };
@@ -83,8 +82,8 @@ const updateTicket = (request, response) => {
   const {
     name,
     email,
-    phoneNum,
-    ext,
+    phone,
+    extension,
     summary,
     type,
     priority,
@@ -94,13 +93,13 @@ const updateTicket = (request, response) => {
   } = request.body;
 
   pool.query(
-    "UPDATE tickets SET name = $2, email = $3, phoneNum = $4, ext = $5, summary = $6, priority = $7, category = $8, subcategory = $9, details = $10 WHERE id = $1",
+    "UPDATE tickets SET name = $2, email = $3, phone = $4, extension = $5, summary = $6, type = $7,  priority = $8, category = $9, subcategory = $10, details = $11 WHERE id = $1",
     [
       id,
       name,
       email,
-      phoneNum,
-      ext,
+      phone,
+      extension,
       summary,
       type,
       priority,
@@ -127,7 +126,7 @@ const deleteUser = (request, response) => {
     if (error) {
       throw error;
     }
-    response.status(200).send(`User deleted with ID: ${id}`);
+    response.status(200).send(`Ticket deleted with ID: ${id}`);
   });
 };
 
